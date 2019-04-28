ea52604a8d05eca742cb74fefedd6ea826eafa20
RichardLiu <sausiliu@163.com>
2019-04-28 16:58:10 +0800

    Added dataExcel.py

--- dataExcel.py
+++ dataExcel.py
@@ -4,7 +4,8 @@
 
 import pandas as pd
 import numpy as np
-#import csv
+
+# import csv
 
 # filename可以直接从盘符开始，标明每一级的文件夹直到csv文件，
 # header=None表示头部为空，sep=' '表示数据间使用空格作为分隔符
@@ -13,12 +14,27 @@ import numpy as np
 # ---------------------
 
 data = pd.read_csv('./database.csv', index_col=0)
+# data = pd.read_csv('./test_database.csv', index_col=0)
 # data = pd.read_csv('./data.csv', index_col=0)
 
 # data.sort_values(['Year', "Happiness Score"], ascending=[True, False], inplace=True)
 
-print('head:')
-print(data.head())
-print('\n\ntail:')
-print(data.tail())
+# print('head:')
+# print(data.head())
+# print('\n\ntail:')
+# print(data.tail())
+
+# table = data.pivot_table(data, index=["Country", "Family"])
+# table = data.pivot_table(data, index=["区县"], values=["发现问题数"])
+# table = data.pivot_table(data, colums=["区县"], values=["发现问题数"])
+# table = data.pivot_table(data, columns=["区县"], index=["发现问题数"], aggfunc=np.sum)
+table = data.pivot_table(data, columns=["区县"], aggfunc=np.sum)
+
+print(table)
+
+# print(table.ix['范县'])
+
+print('\n\n------------:')
+#print(table)
+# table.to_csv('test.csv', encoding='utf_8_sig')
 
