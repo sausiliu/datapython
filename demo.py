4b1d4dba04d5be938d8e1514f01476eaebe081f3
RichardLiu <sausiliu@163.com>
2019-04-29 17:15:43 +0800

    update demo.py

--- demo.py
+++ demo.py
@@ -7,37 +7,16 @@ import numpy as np
 
 # import csv
 
-# filename可以直接从盘符开始，标明每一级的文件夹直到csv文件，
-# header=None表示头部为空，sep=' '表示数据间使用空格作为分隔符
-# 如果分隔符是逗号，只需换成 ‘，’即可。
-# df=pd.read_csv('filename',header=None,sep=' ')
-# ---------------------
-
-# data = pd.read_csv('./database.csv', index_col=0)
 data = pd.read_csv('./sales-funnel.csv', index_col=0)
-# data = pd.read_csv('./test_database.csv', index_col=0)
-# data = pd.read_csv('./data.csv', index_col=0)
-
-# data.sort_values(['Year', "Happiness Score"], ascending=[True, False], inplace=True)
 
 print('head:')
 print(data.head())
 print('\n\ntail:')
 print(data.tail())
 
-# table = data.pivot_table(data, index=["Country", "Family"])
-# table = data.pivot_table(data, index=["区县"], values=["发现问题数"])
-# table = data.pivot_table(data, colums=["区县"], values=["发现问题数"])
-# table = data.pivot_table(data, columns=["区县"], index=["发现问题数"], aggfunc=np.sum)
-table = data.pivot_table(data, index=["区县"], aggfunc=np.sum)
-#table = data.pivot_table(data, index=["区县"], values=["报警次数"])
-print(type(table))
-
-print(table)
-
-# print(table.ix['范县'])
-
 print('\n\n------------:')
-#print(table)
+
+table = pd.pivot_table(data,index=["Manager","Rep"],values=["Price"],aggfunc=np.sum)
+print(table)
 # table.to_csv('test.csv', encoding='utf_8_sig')
 
