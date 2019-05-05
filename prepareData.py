a0f20a752e0b9a6570a1df7a18d2f373b7387a95
RichardLiu <sausiliu@163.com>
2019-05-05 14:40:57 +0800

    update demo.py

--- prepareData.py
+++ prepareData.py
@@ -1,90 +1,83 @@
 import xlwt
-import xlrd
 import pymysql
-import math
-
 
 # 创建数据表SQL语句
-sql = """select * from
-(select MEASURE_DATETIME as 1月,STAND_GRIDID,PM25 as 1_pm25 from SENSOR1.T_GRID_STAT_FOR_CITY_MONTHLY where
-STAND_GRIDID in
-(select  STAND_GRIDID from SENSOR1.T_GRID_OF_INTEREST where
-MEASURE_DATETIME>='2019-01-01'
-and MEASURE_DATETIME<='2019-01-31' and REASON not like '' and DELETE_FLAG=0
-and CITY_ID in
-(select CITY_ID from SENSOR1.T_DICT_CITY_GROUP where GROUP_ID='HOTGRID_11'))
-and MEASURE_DATETIME>='2019-01-01' and MEASURE_DATETIME<='2019-01-31'
-and VAR_TYPE_ID=1 and REGION_LEVEL=0)a
-left join
-(select MEASURE_DATETIME as 2月,STAND_GRIDID,PM25 as 2_pm25 from SENSOR1.T_GRID_STAT_FOR_CITY_MONTHLY where
-STAND_GRIDID in
-(select  STAND_GRIDID from SENSOR1.T_GRID_OF_INTEREST where
-MEASURE_DATETIME>='2019-01-01'
-and MEASURE_DATETIME<='2019-01-31' and REASON not like '' and DELETE_FLAG=0
-and CITY_ID in
-(select CITY_ID from SENSOR1.T_DICT_CITY_GROUP where GROUP_ID='HOTGRID_11'))
-and MEASURE_DATETIME>='2019-02-01' and MEASURE_DATETIME<='2019-02-28'
-and VAR_TYPE_ID=1 and REGION_LEVEL=0)b
-on a.STAND_GRIDID=b.STAND_GRIDID
-left join
-(select MEASURE_DATETIME as 3月,STAND_GRIDID,PM25 as 3_pm25 from SENSOR1.T_GRID_STAT_FOR_CITY_MONTHLY where
-STAND_GRIDID in
-(select  STAND_GRIDID from SENSOR1.T_GRID_OF_INTEREST where
-MEASURE_DATETIME>='2019-01-01'and MEASURE_DATETIME<='2019-01-31'
-and REASON not like '' and DELETE_FLAG=0 and CITY_ID in
-(select CITY_ID from SENSOR1.T_DICT_CITY_GROUP where GROUP_ID='HOTGRID_11'))
-and MEASURE_DATETIME>='2019-03-01' and MEASURE_DATETIME<='2019-03-31'
-and VAR_TYPE_ID=1 and REGION_LEVEL=0 )c
-on a.STAND_GRIDID=c.STAND_GRIDID"""
+# sql = """select * from
+# (select MEASURE_DATETIME as 1月,STAND_GRIDID,PM25 as 1_pm25 from SENSOR1.T_GRID_STAT_FOR_CITY_MONTHLY where
+# STAND_GRIDID in
+# (select  STAND_GRIDID from SENSOR1.T_GRID_OF_INTEREST where
+# MEASURE_DATETIME>='2019-01-01'
+# and MEASURE_DATETIME<='2019-01-31' and REASON not like '' and DELETE_FLAG=0
+# and CITY_ID in
+# (select CITY_ID from SENSOR1.T_DICT_CITY_GROUP where GROUP_ID='HOTGRID_11'))
+# and MEASURE_DATETIME>='2019-01-01' and MEASURE_DATETIME<='2019-01-31'
+# and VAR_TYPE_ID=1 and REGION_LEVEL=0)a
+# left join
+# (select MEASURE_DATETIME as 2月,STAND_GRIDID,PM25 as 2_pm25 from SENSOR1.T_GRID_STAT_FOR_CITY_MONTHLY where
+# STAND_GRIDID in
+# (select  STAND_GRIDID from SENSOR1.T_GRID_OF_INTEREST where
+# MEASURE_DATETIME>='2019-01-01'
+# and MEASURE_DATETIME<='2019-01-31' and REASON not like '' and DELETE_FLAG=0
+# and CITY_ID in
+# (select CITY_ID from SENSOR1.T_DICT_CITY_GROUP where GROUP_ID='HOTGRID_11'))
+# and MEASURE_DATETIME>='2019-02-01' and MEASURE_DATETIME<='2019-02-28'
+# and VAR_TYPE_ID=1 and REGION_LEVEL=0)b
+# on a.STAND_GRIDID=b.STAND_GRIDID
+# left join
+# (select MEASURE_DATETIME as 3月,STAND_GRIDID,PM25 as 3_pm25 from SENSOR1.T_GRID_STAT_FOR_CITY_MONTHLY where
+# STAND_GRIDID in
+# (select  STAND_GRIDID from SENSOR1.T_GRID_OF_INTEREST where
+# MEASURE_DATETIME>='2019-01-01'and MEASURE_DATETIME<='2019-01-31'
+# and REASON not like '' and DELETE_FLAG=0 and CITY_ID in
+# (select CITY_ID from SENSOR1.T_DICT_CITY_GROUP where GROUP_ID='HOTGRID_11'))
+# and MEASURE_DATETIME>='2019-03-01' and MEASURE_DATETIME<='2019-03-31'
+# and VAR_TYPE_ID=1 and REGION_LEVEL=0 )c
+# on a.STAND_GRIDID=c.STAND_GRIDID"""
+
+host = '10.112.115.35'
+user = 'root',
+passwd = '1',
+port = 3306,
 
 
-sql_demo="""SELECT * FROM `performance_schema`.`file_summary_by_event_name` LIMIT 0,1000"""
-
-
-def export_data():
-    # db = pymysql.connect(
-    #     host='10.10.8.52',
-    #     user='xxxxx',
-    #     passwd='Password793',
-    #     port=30002,
-    #     db='SENSOR1',
-    #     charset='utf8')
-
-    db = pymysql.connect(
-        host='192.168.31.74',
-        user='root',
-        passwd='1',
-        port=3306,
-        charset='utf8')
-
-    cursor = db.cursor()  # 建立游标
-
-    # cursor.execute(sql)
-    cursor.execute(sql_demo)
-    # sensor_data = cursor.fetchone()
-    sensor_data = cursor.fetchall()
-    fields = cursor.description
-
-    db.close()
-    print(fields)
-    print("len fields: {}".format(len(fields)))
-
-    print(sensor_data)
-    print("len sensor data: {}".format(len(sensor_data)))
+def export_data(sqls):
 
     # Create xlsx
     excel = xlwt.Workbook()
-
     sheet = excel.add_sheet(u'sheet1', cell_overwrite_ok=True)
 
-    for i in range(len(fields)):
-        print(fields[i][0])
-        sheet.write(0, i, fields[i][0])
+    global_c = 0
 
-    for r in range(len(sensor_data)):
-        for c in range(len(fields)):
-            #  print(sensor_data[r][c])
-            sheet.write(1+r, c, sensor_data[r][c])
+    # 执行多个sql查询语句，并写入excel
+    for sql in sqls:
+        db = pymysql.connect(
+            host='10.112.115.35',
+            user='root',
+            passwd='1',
+            port=3306,
+            charset='utf8')
+
+        cursor = db.cursor()  # 建立游标
+        cursor.execute(sql)
+        data = cursor.fetchall()
+        fields = cursor.description
+        # print("len fields: {}".format(len(fields)))
+        # print("len data: {}".format(len(data)))
+
+        db.close()
+        # 写入行名称
+        for i in range(len(fields)):
+            print(fields[i][0])
+            sheet.write(0, i+global_c, fields[i][0])
+
+        # 写入行名称
+        for r in range(len(data)):
+            for c in range(len(fields)):
+                #  print(sensor_data[r][c])
+                sheet.write(1+r, c+global_c, data[r][c])
+
+        global_c = global_c + len(fields)
+    # sensor_data = cursor.fetchone()
 
     excel.save('test.xls')
 
@@ -119,5 +112,10 @@ if __name__ == "__main__":
     # excel = xlwt.Workbook('text.xlsx')
 
     # sheet = excel.add_sheet(u'sheet1', cell_overwrite_ok=True)
-    export_data()
+    sqls = [
+        """SELECT * FROM `performance_schema`.`file_summary_by_event_name` LIMIT 0,1000""",
+        """SELECT * FROM `performance_schema`.`file_summary_by_event_name` LIMIT 0,1000""",
+        """SELECT * FROM `performance_schema`.`file_summary_by_event_name` LIMIT 0,1000""",
+    ]
+    export_data(sqls)
 
