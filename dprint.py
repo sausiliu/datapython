5ed75c1f20d89bb4ce145bf91a115a45a96c5582
RichardLiu <sausiliu@163.com>
2019-05-22 18:28:30 +0800

    set document's font as 宋体

---
+++ dprint.py
@@ -0,0 +1,23 @@
+import sys
+debug=True
+
+GREEN = lambda x: "\x1b[32;01m" + x + "\x1b[39;49;00m"
+BLUE = lambda x: "\x1b[34;01m" + x + "\x1b[39;49;00m"
+BOLD = lambda x: "\033[1m" + x + "\033[0m"
+YELLOW = lambda x: "\x1b[33;01m" + x + "\x1b[39;49;00m"
+
+
+def dprint(*args):
+
+    if debug:
+
+        s = ""
+        for a in args:
+            s += ("%s" % a)
+
+        co = sys._getframe(1).f_code
+
+        frame = BOLD(co.co_name)
+
+        print("%s %s" % (fname, "(%s:%d)" % (co.co_filename, co.co_firstlineno)))
+        print(s)
