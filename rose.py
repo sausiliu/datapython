57e75becffbbf4ab1cbeab7b277113a403cc5842
RichardLiu <sausiliu@163.com>
2019-06-14 11:14:33 +0800

    update rose

--- rose.py
+++ rose.py
@@ -1,22 +1,16 @@
 from windrose import WindroseAxes
 import pandas as pd
-from matplotlib import pyplot as plt
-import matplotlib.cm as cm
 import numpy as np
 
 # data = pd.read_csv('./wind.csv', index_col=0)
 # data = pd.read_csv('./wind.csv', usecols=[0])
-data = pd.read_csv('./wind.csv')
-#data_matrix = data.as_matrix()
+# 读取第一列
+data = pd.read_csv('./wind.csv', usecols=[0])
+# 行列矩阵转换一下
 data_values = data.values
-# data_array = np.array(data)
-# print(data_array)
-#print(data_values)
 print(type(data_values))
-
 print(np.transpose(data_values)[0])
 
-# print(map(list, zip(*data_values))[0])
 
 # 使用nmupy随机生成风速风向数组
 # ws = np.random.random(500) * 6
